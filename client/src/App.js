4c3e0283036f3aa7ba9dcf60548dc296dbbc0ddc
Arafin Mridul <mridularafin@gmail.com>
2024-01-04 17:19:45 +0600

    skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+import send from "./assets/send.svg";
 
-function App() {
-  return <h1>Hello React 18</h1>;
-}
+const App = () => {
+  const chatContainerRef = useRef(null);
+  const formRef = useRef(null);
+
+  return (
+    <div id="app">
+      <div ref={chatContainerRef} id="chat_container"></div>
+      <form ref={formRef}>
+        <textarea
+          name="prompt"
+          rows="1"
+          cols="1"
+          placeholder="Message StellarBot..."
+        ></textarea>
+        <button type="submit">
+          <img src={send} alt="send" />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
